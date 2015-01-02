4f30f71fcc2e162206454cd84b5999229d45ee84
cnbeining <cnbeining@gmail.com>
2015-01-01 20:46:18 -0500

    push py2

--- README.md
+++ README.md
@@ -1,28 +1,42 @@
 AcUpload
 ==========
+
 Acfun 乐视云上传(Beta)脚本，方便折腾黑科技的熊孩子们。
+
+对于经常IO错误者有奇效。
 
 Usage
 --------
-需要Python3。
 
-cookie文件名是./accookies  .
+需要Python2。
 
-格式：`Hm_lvt_49129538199e82fd3d7af**********=1398******; bdshare_firstime=*****66321693; Hm_lvt_bc75b9260fe72ee**********=139826***; auth_key=******; auth_key_ac_sha1=-*****; ac_username=****; ac_userimg=http%3A%2F%2Fstatic.acfun.com%2Fdotnet%2F20120923%2Fstyle%2Fimage%2Favatar.jpg; ac_member_guide=0%2C0%2*****; clientlanguage=zh_CN`
+cookie文件名是```./accookies  ```.
+x
+格式：```Hm_lvt_49129538199e82fd3d7af**********=1398******; bdshare_firstime=*****66321693; Hm_lvt_bc75b9260fe72ee**********=139826***; auth_key=******; auth_key_ac_sha1=-*****; ac_username=****; ac_userimg=http%3A%2F%2Fstatic.acfun.com%2Fdotnet%2F20120923%2Fstyle%2Fimage%2Favatar.jpg; ac_member_guide=0%2C0%2*****; clientlanguage=zh_CN```
 
-运行 `acupload.py [FILENAME1] [FILENAME2]` 开始上传. 
+运行 ```acupload.py [FILENAME1] [FILENAME2]``` 开始上传. 
 
-上传完毕后会自动进行检验。
+上传完毕后会自动进行检验。但是对于巨大文件有可能不准。
 
 文件大小是胡写的，理论上可以上传巨大文件。但是具体啥样就不知道了。死了别找我。
 
 
 Misc
 ----
+
 * 晚上写的，肚子饿了，有问题谅解，也没做PEP-8，随便看看吧。
 * 禁止转载到**墙内**各种电子公告服务（包括但不限于百毒贴吧、AC 丧尸岛、各种论坛、微博等）。发现腿打折。之前不是没有过先例。
+
+
+License
+----
+
+GPL v3.
+
+
 
 #### `vu` 的用法
 * <http://yuntv.letv.com/bcloud.swf?uu=iamuseless&vu=cd09b2f515>
 * `[localup]cd09b2f515[/localup]`
 * 乐视云视频 - cd09b2f515
+* https://github.com/cnbeining/letvcloudchecker
--- acupload.py
+++ acupload.py
@@ -1,94 +1,95 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #coding:utf-8
 # Author:  Beining --<ACICFG>
 # Purpose:  Upload to Letv via Acfun's API
-# Created: 07/17/2014
+# Created: 01/01/2015
 
-#python3 acup.py 0.flv 1.flv
+#python acup.py 0.flv 1.flv
 
-import urllib.request
+import urllib2, urllib
 import sys
 import os
 import json
 import subprocess
 import hashlib
+from io import open
 
 global cookiepath
-cookiepath = './accookies'
+cookiepath = u'./accookies'
 global vu_list
 vu_list = []
 
 #----------------------------------------------------------------------
 def check_upload(source_id):
-    """"""
-    str2Hash = 'cfflashformatjsonran0.7214574650861323uu2d8c027396ver2.1vu' + source_id + 'bie^#@(%27eib58'
-    sign = hashlib.md5(str2Hash.encode('utf-8')).hexdigest()
-    request_info = urllib.request.Request('http://api.letvcloud.com/gpc.php?&sign='+sign+'&cf=flash&vu='+source_id+'&ver=2.1&ran=0.7214574650861323&qr=2&format=json&uu=2d8c027396')
+    u""""""
+    str2Hash = u'cfflashformatjsonran0.7214574650861323uu2d8c027396ver2.1vu' + source_id + u'bie^#@(%27eib58'
+    sign = hashlib.md5(str2Hash.encode(u'utf-8')).hexdigest()
+    request_info = urllib2.Request(u'http://api.letvcloud.com/gpc.php?&sign='+sign+u'&cf=flash&vu='+source_id+u'&ver=2.1&ran=0.7214574650861323&qr=2&format=json&uu=2d8c027396')
     try:
-        response = urllib.request.urlopen(request_info)
+        response = urllib2.urlopen(request_info)
         data = response.read()
-        return json.loads(data.decode('utf-8'))['message']
+        return json.loads(data.decode(u'utf-8'))[u'message']
     except:
-        return 'Cannot check '+source_id+' !'
+        return u'Cannot check '+source_id+u' !'
 
 
 #----------------------------------------------------------------------
 def upload(file2Upload):
-    """"""
+    u""""""
     #Read Cookie.....Damn it I didn't have my supper!
     try:
-        cookies = open(cookiepath, 'r').readline()
+        cookies = open(cookiepath, u'r').readline()
         #print(cookies)
     except:
-        print('I am hungry, please give me your Cookie!')
+        print u'I am hungry, please give me your Cookie!'
         exit()
     #Get filename
-    filename = file2Upload.split('/')[-1].strip()
+    filename = file2Upload.split(u'/')[-1].strip()
     #print(filename)
     if not os.path.isfile(file2Upload):
-        print('Not file!')
+        print u'Not file!'
         pass
     #Calculate Filesize
     #Not used to upload extreme large file, may cause problem
     #filesize = os.path.getsize(file2Upload)
     #print(filesize)
     #Fetch UploadUrl
-    request_full = urllib.request.Request('http://www.acfun.tv/video/createVideo.aspx?type=letv&filesize=100000', headers={ 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36', 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' , 'Cookie': cookies,})
+    request_full = urllib2.Request(u'http://www.acfun.tv/video/createVideo.aspx?type=letv&filesize=100000', headers={ u'User-Agent' : u'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36', u'Cache-Control': u'no-cache', u'Pragma': u'no-cache' , u'Cookie': cookies,})
     try:
-        response = urllib.request.urlopen(request_full)
+        response = urllib2.urlopen(request_full)
     except Exception:
-        print('Cannot get response from server!')
+        print u'Cannot get response from server!'
         pass
     data = response.read()
-    uploadresponse = json.loads(data.decode('utf-8'))
-    if uploadresponse["success"] is 'false':
-        print('ERROR: '+ uploadresponse['info'])
+    uploadresponse = json.loads(data.decode(u'utf-8'))
+    if uploadresponse[u"success"] is u'false':
+        print u'ERROR: '+ uploadresponse[u'info']
         exit()
     #print(uploadresponse['upload_url'])
     #make filename
-    remote_name = uploadresponse['sourceId'] + '|' + str(filesize)
-    status_url = uploadresponse['progress_url']
-    source_id = uploadresponse['sourceId']
+    remote_name = uploadresponse[u'sourceId'] + u'|' + unicode(filesize)
+    status_url = uploadresponse[u'progress_url']
+    source_id = uploadresponse[u'sourceId']
     #start upload
-    upload_url = str(uploadresponse['upload_url'])
+    upload_url = unicode(uploadresponse[u'upload_url'])
     #print(upload_url)
-    os.system('curl   -F \"file=@'+filename+'\"  \"'+upload_url+'\" | cat #')
-    print('\n'+'Hope everything is fine. '+source_id)
+    os.system(u'curl   -F \"file=@'+filename+u'\"  \"'+upload_url+u'\" | cat #')
+    print u'\n'+u'Hope everything is fine. '+source_id
     vu_list.append([filename, source_id])
 
 #----------------------------------------------------------------------
 def cmd_exists(cmd):
-    return subprocess.call("type " + cmd, shell=True, 
+    return subprocess.call(u"type " + cmd, shell=True, 
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 #----------------------------------------------------------------------
-if __name__=='__main__':
+if __name__==u'__main__':
     #Test sys encoding
-    if not sys.getdefaultencoding() is 'utf-8':
-        os.system('export LC_ALL="en_US.UTF-8"')
+    if not sys.getdefaultencoding() is u'utf-8':
+        os.system(u'export LC_ALL="en_US.UTF-8"')
     #Test curl
-    if not cmd_exists('curl'):
-        print ('We need curl to upload your file! Get one with apt-get or yum.')
+    if not cmd_exists(u'curl'):
+        print u'We need curl to upload your file! Get one with apt-get or yum.'
         exit()
 
     total_file_num = len(sys.argv[1:])
@@ -96,8 +97,8 @@ if __name__=='__main__':
     for name in sys.argv[1:]:
         #print(name)
         i = i + 1
-        print('Uploading '+ str(i)+' in '+str(total_file_num)+' files...')
+        print u'Uploading '+ unicode(i)+u' in '+unicode(total_file_num)+u' files...'
         upload(name)
-    #os.system('export LC_ALL="en_US.UTF-8"')
+    os.system(u'export LC_ALL="en_US.UTF-8"')
     for media in vu_list:
-        print(media[0] + ',' + media[1] + ','+check_upload(media[1]))
+        print media[0] + u', ' + media[1] + u', '+check_upload(media[1])
